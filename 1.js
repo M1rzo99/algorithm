842ea98ab6cddc3da3ac3d5155b82ca8ed6978a6
Mirzo <mirzo@example.com>
2025-07-21 22:23:56 +0900

    FEAT: learning algorithms with Sariq Dev

---
+++ 1.js
@@ -0,0 +1,23 @@
+// *1 algorithm
+// const a = 1
+// const b = 2
+
+// summa = a + b
+// console.log(summa);
+
+// *2 algorithm
+// const a = 300;
+// const b = 300;
+// const c = 300;
+
+// if (a >= b && a >= c) {
+//     console.log("A eng kattasi");
+// } else if (b >= a && b >= c) {
+//     console.log("B eng kattasi");
+// } else {
+//     console.log("C eng kattasi");
+// }
+
+// *3 algorithm
+
+let n = 1
