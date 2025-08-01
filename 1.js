8dcb906d14a09f4fbfeb60ea4626eacfdc973baa
Mirzo <mirzo@example.com>
2025-08-01 23:17:24 +0900

    feat: learning algorith

--- 1.js
+++ 1.js
@@ -135,3 +135,25 @@
 // const result = mostFrequentChar(input);
 
 // console.log("Eng ko'p uchraydigan harf:", result);
+
+function lengthOfLongestSubstring(s) {
+    let seen = new Set();
+    let left = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
+        }
+        seen.add(s[right]);
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
+
+// Sinov
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
